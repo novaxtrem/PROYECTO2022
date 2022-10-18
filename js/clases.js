19eb10c0f43b93fbf70b4ce74bab4625d297d6a2
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2022-10-17 23:17:14 -0300

    up

--- js/clases.js
+++ js/clases.js
@@ -1,24 +1,28 @@
 
 class Producto {
-    constructor(producto_id, producto_nombre, producto_descripcion, producto_stock, producto_precio, producto_categoria_id) {
+    constructor(producto_id, producto_nombre, producto_categoria, producto_descripcion, producto_precio, producto_stock, producto_locacion_logitud, producto_locacion_latitud, producto_locacion_alias, producto_imagen) {
         this.producto_id = producto_id;
         this.producto_nombre = producto_nombre;
+        this.producto_categoria = producto_categoria;
         this.producto_descripcion = producto_descripcion;
-        this.producto_stock = producto_stock;
         this.producto_precio = producto_precio;
-        this.producto_categoria_id = producto_categoria_id;
+        this.producto_stock = producto_stock;
+        this.producto_locacion_logitud = producto_locacion_logitud;
+        this.producto_locacion_latitud = producto_locacion_latitud;
+        this.producto_locacion_alias = producto_locacion_alias;
+        this.producto_imagen = producto_imagen;
+
     }
 }
 //////////////////////////////
 class Usuario {
-    constructor(usuario_email, usuario_contrasenia, usuario_nombre, usuario_direccion, usuario_telefono, usuario_QR_mercado_libre, usuario_comercio, usuario_estado) {
+    constructor(usuario_email, usuario_contrasenia, usuario_nombre, usuario_direccion, usuario_telefono, usuario_QR_mercado_libre, usuario_estado) {
         this.usuario_email = usuario_email;
         this.usuario_contrasenia = usuario_contrasenia;
         this.usuario_nombre = usuario_nombre;
         this.usuario_direccion = usuario_direccion;
         this.usuario_telefono = usuario_telefono;
         this.usuario_QR_mercado_libre = usuario_QR_mercado_libre;
-        this.usuario_comercio = usuario_comercio;
         this.usuario_estado = usuario_estado;
     }
 }
--- php/agrego-productos.php
+++ php/agrego-productos.php
@@ -5,12 +5,16 @@ header('Access-Control-Allow-Origin: *');
 
 $id = $_POST['producto_id'];
 $nombre = $_POST['producto_nombre'];
+$categoria = $_POST['producto_categoria'];
 $descripcion = $_POST['producto_descripcion'];
-$stock = $_POST['producto_stock'];
 $precio = $_POST['producto_precio'];
-$categoria_id = $_POST['producto_categoria_id'];
+$stock = $_POST['producto_stock'];
+$locacion_logitud = $_POST['producto_locacion_logitud'];
+$locacion_latitud = $_POST['producto_locacion_latitud'];
+$locacion_alias = $_POST['producto_locacion_alias'];
+$producto_imagen = $_POST['producto_imagen'];
 //
-$q = "INSERT INTO `productos`(`producto_id`, `producto_nombre`, `producto_descripcion`, `producto_stock`, `producto_precio`, `producto_categoria_id`) VALUES ('$id','$nombre','$descripcion','$stock','$precio','$categoria_id');";
+$q = "INSERT INTO `productos`(`producto_id`, `producto_nombre`, `producto_categoria`, `producto_descripcion`, `producto_precio`, `producto_stock`, `producto_locacion_logitud`, `producto_locacion_latitud`, `producto_locacion_alias`, `producto_imagen`) VALUES ('$id','$nombre','$descripcion','$precio','$stock','$locacion_logitud','$locacion_latitud','$locacion_alias','$producto_imagen');";
 $query = mysqli_query($conn, $q);
 //
 if ($query) {
