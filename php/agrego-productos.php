fb08b4bfa96cabcc533a8ca8ca2efbe9c3d41f96
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2022-10-19 20:44:06 -0300

    sss

--- php/agrego-productos.php
+++ php/agrego-productos.php
@@ -3,7 +3,6 @@ require 'coneccion.php';
 header('Access-Control-Allow-Origin: *');
 //
 
-
 $nombre = $_POST['producto_nombre'];
 $categoria = $_POST['producto_categoria'];
 $descripcion = $_POST['producto_descripcion'];
@@ -14,9 +13,13 @@ $locacion_latitud = $_POST['producto_locacion_latitud'];
 $locacion_alias = $_POST['producto_locacion_alias'];
 $producto_imagen = $_POST['producto_imagen'];
 //
-$q = "INSERT INTO `productos`( `producto_nombre`, `producto_categoria`, `producto_descripcion`, `producto_precio`, `producto_stock`, `producto_locacion_logitud`, `producto_locacion_latitud`, `producto_locacion_alias`, `producto_imagen`) VALUES ($nombre','$descripcion','$precio','$stock','$locacion_logitud','$locacion_latitud','$locacion_alias','$producto_imagen');";
+$q = "INSERT INTO `productos`(`producto_id`, `producto_nombre`, `producto_categoria`, `producto_descripcion`, `producto_precio`, `producto_stock`, `producto_locacion_logitud`, `producto_locacion_latitud`, `producto_locacion_alias`, `producto_imagen`) VALUES (DEFAULT,'$nombre','$descripcion','$precio','$stock','$locacion_logitud','$locacion_latitud','$locacion_alias','$producto_imagen');";
+
 $query = mysqli_query($conn, $q);
 //
+
+
+
 if ($query) {
     echo json_encode("ok");
     //echo json_encode("Data Inserted Successfully");
