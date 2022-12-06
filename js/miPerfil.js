b8b2dda32287b25638b9ceb7acfe5b789ab74762
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2022-12-05 23:56:44 -0300

    up

--- js/miPerfil.js
+++ js/miPerfil.js
@@ -1,75 +1,75 @@
 
 
-var usuarioConectado = "";
 
 $(document).ready(function () {
+    var usuarioConectado = "";
     usuarioConectado = JSON.parse(localStorage.getItem('USUARIO_CONECTADO'));
     //
     dibujoPerfil();
-
 
     $("#inputFileToLoad").change(function () {
         encodeImageFileAsURL(this);
     });
 
 
-    /*
-        $('#cerrar-sesion').click(function () {
-            localStorage.clear();
-            window.location.href = 'index.html';
-        });
-    */
 });
 
 
 function dibujoPerfil() {
     var htmlContentToAppend = "";
-
+    //
+    
     htmlContentToAppend += `
         <div class="row">
-            <div class="col-md-3 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                    <!--MOVIDA DE LA IMAGEN-->
-                    <div width="300px" id="imgTest"></div>
-                    <input  id="inputFileToLoad" class="form-control" type="file"/>
-                    <!--MOVIDA DE LA IMAGEN FIN-->       
-                    <span class="text-black-50">` + usuarioConectado.usuario_email + `</span><span> </span>
-                </div>
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <h4 class="text-right">Mis datos</h4>
             </div>
-            <div class="col-md-5 border-right">
-                <div class="p-3 py-6">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="text-right">Mis datos</h4>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12"><label class="labels">Nombre</label>
-                            <input type="text" class="form-control"  value="` + usuarioConectado.usuario_nombre + `">
-                            <br>
-                        </div>
-                        <div class="col-md-12">
-                            <label class="labels">Direccion</label>
-                            <input type="text" class="form-control" value="` + usuarioConectado.usuario_direccion + `">
-                            <br>
-                        </div>
-                        <div class="col-md-12">
-                            <label class="labels">Telefono</label>
-                            <input type="text" class="form-control"  value="` + usuarioConectado.usuario_telefono + `">
-                            <br>
-                        </div>
-                        <div class="col-md-12">
-                            <label class="labels"></label>
-                            <button type="button" class="btn btn-outline-dark"style="margin-top: 10px;margin-left: 10px;">Mis Compras</button>
-                            <button type="button" class="btn btn-outline-dark"style="margin-top: 10px;margin-left: 10px;">Mis Ventas</button>
-                            <button type="button" class="btn btn-outline-dark"style="margin-top: 10px;margin-left: 10px;">Mis Productos</button>
-                        </div>
-                    <div class="mt-5 text-center">
-                        <button class="btn btn-warning profile-button" type="button">Guardar</button>
-                    </div>
-                </div>
+            
+            <div class="col-md-12">
+            <!--MOVIDA DE LA IMAGEN-->
+            <label class="labels">QR Mercado libre</label>
+            <div width="300px" id="imgTest"></div>
+            <input  id="inputFileToLoad" class="form-control" type="file"/>
+            <!--MOVIDA DE LA IMAGEN FIN-->       
+            <br>
+            </div>
+            <div class="col-md-12">
+                <label class="labels">Nombre</label>
+                <input type="text" class="form-control"  value="` + usuarioConectado.usuario_nombre + `">
+                <br>
+            </div>
+            <div class="col-md-12">
+                <label class="labels">email</label>
+                <span class="text-black-50">` + usuarioConectado.usuario_email + `</span>
+            </div>
+            <div class="col-md-12">
+                <label class="labels">Direccion</label>
+                <input type="text" class="form-control" value="` + usuarioConectado.usuario_direccion + `">
+                <br>
+            </div>
+            <div class="col-md-12">
+                <label class="labels">Telefono</label>
+                <input type="text" class="form-control"  value="` + usuarioConectado.usuario_telefono + `">
+                <br>
+            </div>
+            <div class="col-md-10">
+                <label class="labels"></label>
+                <button type="button" class="btn btn-outline-dark"style="margin-top: 10px;margin-left: 10px;">Mis Compras</button>
+                <button type="button" class="btn btn-outline-dark"style="margin-top: 10px;margin-left: 10px;">Mis Ventas</button>
+                <button type="button" class="btn btn-outline-dark"style="margin-top: 10px;margin-left: 10px;">Mis Productos</button>
+            </div>
+            <div class="mt-5 text-center">
+                <button class="btn btn-warning profile-button" type="button">Guardar</button>
             </div>
         </div>`;
 
     document.getElementById("contenedor-perfil").innerHTML = htmlContentToAppend;
+    //
+    var imgMeli = document.createElement('img');
+    imgMeli.style.cssText += 'max-width: 300px;';
+    imgMeli.src = usuarioConectado.usuario_QR_mercado_libre;
+    document.getElementById("imgTest").innerHTML = imgMeli.outerHTML;
+    //
 }
 
 function encodeImageFileAsURL() {
