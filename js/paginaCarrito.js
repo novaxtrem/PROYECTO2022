409fbea6ce2706d7094301c43e82df835db47305
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2022-12-06 09:12:29 -0300

    up

--- js/paginaCarrito.js
+++ js/paginaCarrito.js
@@ -95,52 +95,57 @@ function calculoCostoCarrito() {
 
 function dibujoCarrito() {
 
-
     listaProductosCarrito = JSON.parse(localStorage.getItem('CARRITO'));
     var htmlContentToAppend = "";
 
-    for (var i = 0; i < listaProductosCarrito.length; i++) {
-        htmlContentToAppend +=
-            `<div class="producto">
-                <div class="row justify-content-center align-items-center">
-                    <div class="col-md-3">
-                        <div class="product-image">
-                            <img class="img-fluid d-block mx-auto image imagen-producto" src="`+ listaProductosCarrito[i].producto_imagen + `">
+
+
+    if (listaProductosCarrito == null) {
+        htmlContentToAppend =
+            `<div>
+                    <h1 style="color:#f29c40;text-align:center;padding-top:15px">no hay productos en el carrito</h1>
+                </div>`
+        document.getElementById("contenedor-principal").innerHTML = htmlContentToAppend;
+
+    } else {
+
+        for (var i = 0; i < listaProductosCarrito.length; i++) {
+            htmlContentToAppend +=
+                `<div class="producto">
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-md-3">
+                            <div class="product-image">
+                                <img class="img-fluid d-block mx-auto image imagen-producto" src="`+ listaProductosCarrito[i].producto_imagen + `">
+                            </div>
+                        </div>
+                        <div class="col-md-5 product-info">
+                            <a class="nombre-producto" style="color: rgb(253,157,13);">` + listaProductosCarrito[i].producto_nombre + `</a>
+                            <p class="id-producto" style="display:none">`+ listaProductosCarrito[i].producto_id + `</p>
+                            <button class="btn btn-primary boton-eliminar"style="background-color: rgb(253,157,13);">eliminar</button>
+                        </div>
+                        <div class="col-6 col-md-2 cantidad-productos">
+                            <label class="form-label d-none d-md-block" for="quantity">Cantidad</label>
+                            <input type="number" class="form-control quantity-input catidad-productos-agregados" value="`+ listaProductosCarrito[i].producto_catidad_agregados_compra + `">
+                        </div>
+                        <div class="col-6 col-md-2 contenedor-precio-producto">
+                            <label class="form-label d-none d-md-block" for="precio-unitario">precio</label>
+                            <span class="precio-unitario">$ `+ listaProductosCarrito[i].producto_precio + `</span>
                         </div>
                     </div>
-                    <div class="col-md-5 product-info">
-                        <a class="nombre-producto" style="color: rgb(253,157,13);">` + listaProductosCarrito[i].producto_nombre + `</a>
-                        <p class="id-producto" style="display:none">`+ listaProductosCarrito[i].producto_id + `</p>
-                        <button class="btn btn-primary boton-eliminar"style="background-color: rgb(253,157,13);">eliminar</button>
-                    </div>
-                    <div class="col-6 col-md-2 cantidad-productos">
-                        <label class="form-label d-none d-md-block" for="quantity">Cantidad</label>
-                        <input type="number" class="form-control quantity-input catidad-productos-agregados" value="`+ listaProductosCarrito[i].producto_catidad_agregados_compra + `">
-                    </div>
-                    <div class="col-6 col-md-2 contenedor-precio-producto">
-                        <label class="form-label d-none d-md-block" for="precio-unitario">precio</label>
-                        <span class="precio-unitario">$ `+ listaProductosCarrito[i].producto_precio + `</span>
-                    </div>
-                </div>
-            </div>`
-        document.getElementById("contenedor-productos-en-el-carrito").innerHTML = htmlContentToAppend;
+                </div>`
+            document.getElementById("contenedor-productos-en-el-carrito").innerHTML = htmlContentToAppend;
+        }
     }
 }
 
-
-
 function agregoOrdenCompra() {
-
-
     $.ajax({
         url: ALTA_ORDEN_COMPRA,
         type: "post",
         data: { orden_compra_id: orden_compra_id, orden_compra_vendedor_id: orden_compra_vendedor_id, orden_compra_comprador_id: orden_compra_comprador_id, orden_compra_numero_operacion_mercado_pago: orden_compra_numero_operacion_mercado_pago, orden_compra_direccion_envio: orden_compra_direccion_envio, orden_compra_costo_envio: orden_compra_costo_envio, orden_compra_total: orden_compra_total, orden_compra_estado: orden_compra_estado, productos_comprados: JSON.stringify(listaFinalProductosComprados) },
         success: function (data) {
-
             console.log(data);
         }
-
     });
 
 }
