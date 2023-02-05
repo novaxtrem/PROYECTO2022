8c8db0c77ce36bf2d50e4e0d8862283acbd51847
StephanieBarbitta <54106941+StephanieBarbitta@users.noreply.github.com>
2023-02-05 03:05:07 -0300

    Contraseña MD5 al actualizar Mis Datos

--- php/actualizo-usuario.php
+++ php/actualizo-usuario.php
@@ -11,7 +11,7 @@ $usuario_telefono = $_POST['usuario_telefono'];
 $usuario_QR_mercado_libre = $_POST['usuario_QR_mercado_libre'];
 $usuario_email = $_POST['usuario_email'];
 //
-$query = "UPDATE `usuarios` SET `usuario_contrasenia`='$usuario_contrasenia', `usuario_nombre`='$usuario_nombre', `usuario_direccion`='$usuario_direccion',`usuario_telefono`='$usuario_telefono',`usuario_QR_mercado_libre`='$usuario_QR_mercado_libre' WHERE `usuario_email`='$usuario_email';";
+$query = "UPDATE `usuarios` SET `usuario_contrasenia`= md5('$usuario_contrasenia'), `usuario_nombre`='$usuario_nombre', `usuario_direccion`='$usuario_direccion',`usuario_telefono`='$usuario_telefono',`usuario_QR_mercado_libre`='$usuario_QR_mercado_libre' WHERE `usuario_email`='$usuario_email';";
 //
 $resultado = mysqli_query($conn, $query);
 //
